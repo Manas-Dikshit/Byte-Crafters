290f4e3fca0559e9bb08f581ced20936f45a44d3
Manas Ranjan Dikshit <manasdikshit48@gmail.com>
2025-02-20 00:13:39 +0530

    Add files via upload

--- build_files.sh
+++ build_files.sh
@@ -1,9 +1,9 @@
 echo "BUILD START"
 # create a virtual environment named 'venv' if it doesn't already exist
-python3.9 -m venv venv
+python3.12 -m venv venv
 
 # activate the virtual environment
 source venv/bin/activate
 pip install -r requirements.txt
-python3.9 manage.py collectstatic
+python3.12 manage.py collectstatic
 echo "BUILD END"
